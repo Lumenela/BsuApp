923944759501df2db7c8e9f1b65442a5d2508c0f
Svetlana <lumenela@gmail.com>
2014-05-21 10:25:09 +0300

    initial

---
+++ Controllers/MyServerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BsuApp.PaymentServiceReference;
+
+namespace BsuApp.Controllers
+{
+    public class MyServerController : Controller
+    {
+        //
+        // GET: /MyServer/
+        public ActionResult Index(string token)
+        {
+            return View();
+        }
+
+        public bool CanUse(string token)
+        {
+            if (String.IsNullOrEmpty(token))
+            {
+                return false;
+            }
+            RegisterClient client = new RegisterClient();
+            bool isPaid = client.IsPaid(token, "1c67d8d3-4209-4bdd-ba88-b33230e3b3e2");
+            return isPaid;
+        }
+	}
+}
